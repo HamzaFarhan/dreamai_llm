bd61bc273a925f72d8fbf8380099d7330034e6a2
Hamza Farhan <thehamza96@gmail.com>
2023-10-01 04:27:33 +0500

    token

--- run_llm.sh
+++ run_llm.sh
@@ -6,6 +6,7 @@ create_variables ray_config.yaml
 llm_fn=${applications__deployments__user_config_llm_config_llm_fn:="null"}
 model_name=${applications__deployments__user_config_llm_config_model_name:="null"}
 url=${applications__deployments__user_config_llm_config_model_url:="http://localhost:8000/v1"}
+token=${applications__deployments__user_config_llm_config_model_token:="null"}
 
 # Extract the protocol, host, and port
 protocol=$(echo "$url" | awk -F '://' '{print $1}')
@@ -22,6 +23,7 @@ echo "host_port: $host_port"
 echo "host: $host"
 echo "port: $port"
 
+
 if [ "$llm_fn" != "vllm" ]; then
     echo "Invalid llm_fn. It must be vllm"
     echo "llm_fn: $llm_fn"
@@ -29,6 +31,6 @@ elif [ "$model_name" == "null" ]; then
     echo "Invalid model name. It must be a valid model name"
     echo "Model Name: $model_name"
 else
-    command="python -m vllm.entrypoints.openai.api_server --model $model_name --host $url --port $port"
+    command="python -m vllm.entrypoints.openai.api_server --model $model_name --host $url --port $port --token $token"
     $command
 fi
